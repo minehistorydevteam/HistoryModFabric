070fbe7d64e0198b97bb1e76fe48d757df3b47c3
fx <fx.e.morin@gmail.com>
2019-05-28 21:15:15 -0400

    Initial commit

---
+++ src/main/java/com/historydevteam/historymod/HistoryMod.java
@@ -0,0 +1,10 @@
+package com.historydevteam.historymod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class HistoryMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
