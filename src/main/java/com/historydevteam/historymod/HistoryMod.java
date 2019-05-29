80101ea178eb2fdfd5f7f6476f8669f2848dd4aa
DeadlyMC <modding.deadlymc@gmail.com>
2019-05-29 13:20:42 +0530

    added creative tabs

--- src/main/java/com/historydevteam/historymod/HistoryMod.java
+++ src/main/java/com/historydevteam/historymod/HistoryMod.java
@@ -1,10 +1,19 @@
 package com.historydevteam.historymod;
 
+import com.historydevteam.historymod.items.StoneWheelItem;
 import com.historydevteam.historymod.registry.HistoryModRegistry;
+import com.historydevteam.historymod.utils.Reference;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
 
 public class HistoryMod implements ModInitializer
 {
+    public static final ItemGroup HISTORY_MOD = FabricItemGroupBuilder.build(
+            new Identifier(Reference.MOD_ID, "historymod"), () -> new ItemStack(StoneWheelItem.getStoneWheelItem()));
+    
     @Override
     public void onInitialize()
     {
--- src/main/java/com/historydevteam/historymod/items/StoneWheelItem.java
+++ src/main/java/com/historydevteam/historymod/items/StoneWheelItem.java
@@ -1,5 +1,6 @@
 package com.historydevteam.historymod.items;
 
+import com.historydevteam.historymod.registry.HistoryModRegistry;
 import net.minecraft.item.Item;
 
 public class StoneWheelItem extends Item
@@ -7,5 +8,10 @@ public class StoneWheelItem extends Item
     public StoneWheelItem(Settings settings)
     {
         super(settings);
+    }
+    
+    public static Item getStoneWheelItem()
+    {
+        return HistoryModRegistry.STONE_WHEEL;
     }
 }
