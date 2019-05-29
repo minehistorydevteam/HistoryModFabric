c02d524077cb41e779106ce88b98216e6d8ed157
DeadlyMC <modding.deadlymc@gmail.com>
2019-05-29 13:25:03 +0530

    added stone wheel to historymod creative tab

--- src/main/java/com/historydevteam/historymod/registry/HistoryModRegistry.java
+++ src/main/java/com/historydevteam/historymod/registry/HistoryModRegistry.java
@@ -1,15 +1,15 @@
 package com.historydevteam.historymod.registry;
 
+import com.historydevteam.historymod.HistoryMod;
 import com.historydevteam.historymod.items.StoneWheelItem;
 import com.historydevteam.historymod.utils.Reference;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class HistoryModRegistry
 {
-    public static final Item STONE_WHEEL = new StoneWheelItem(new Item.Settings().itemGroup(ItemGroup.MISC));
+    public static final Item STONE_WHEEL = new StoneWheelItem(new Item.Settings().itemGroup(HistoryMod.HISTORY_MOD));
     
     public static void registerItems()
     {
