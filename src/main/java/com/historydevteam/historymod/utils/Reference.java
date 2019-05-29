1e23115da7ec756fd77c79ecc507b469f52d6551
DeadlyMC <modding.deadlymc@gmail.com>
2019-05-29 12:26:12 +0530

    added utils

---
+++ src/main/java/com/historydevteam/historymod/utils/Reference.java
@@ -0,0 +1,8 @@
+package com.historydevteam.historymod.utils;
+
+public class Reference
+{
+    public static final String NAME = "HistoryMod";
+    public static final String MOD_ID = "historymod";
+    public static final String MOD_VERSION = "1.0.0";
+}
